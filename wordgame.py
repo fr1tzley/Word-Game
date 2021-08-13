16b83a41b49ee2e11d63fc31c5dac001606c6021
fr1tzley <imbusyreading@gmail.com>
2021-08-12 18:18:42 -0700

    Added weighted randomness and a scorecard

--- wordgame.py
+++ wordgame.py
@@ -1,4 +1,5 @@
 from random import randint
+from numpy.random import choice
 from tkinter.constants import X
 import string
 import PySimpleGUI as sg
@@ -9,11 +10,20 @@ nltk.download('wordnet')
 alphabet_dict = list(string.ascii_uppercase)
 repeats = 5
 word = ""
-#checker = new SpellChecker
+score = 0
 
 
+#The weights of letters in the English language, sourced from below:
+#http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+weights = [8.12, 1.49, 2.71, 4.32, 12.02, 2.30, 2.03, 5.92, 7.31, 0.10, 0.69, 3.98, 2.61, 6.95, 7.68, 1.82, 0.11, 6.02, 6.28, 9.10, 2.88, 1.11, 2.09, 0.17, 2.11, 0.07]
+
+#a dictionary of letters with their scrabble scores in alphabetical order, sourced from below:
+#https://scrabble.hasbro.com/en-us/faq
+scores = {"A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L": 4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6, "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1}
+
+#An empty list to be filled with buttons.
 grid = []
-preserved_grid = []
+
 
 class Handler:
     def __init__(self, x, y, letter):
@@ -33,8 +43,8 @@ def create_grid():
 def make_row():
     list = []
     for int in range(1, repeats + 1):
-        index = randint(1, 26)
-        list.append(alphabet_dict[index - 1])
+        item = choice(alphabet_dict, 1, weights)[0]
+        list.append(item)
     return list
 
 
@@ -73,8 +83,6 @@ def make_coords(x, y):
 
     
 def handle_input(x, y, letter): 
-    
-   
     global word 
     cleaned_letter = "".join([i for i in letter if not i.isdigit()])
     word += cleaned_letter
@@ -110,6 +118,15 @@ def enable_all():
             button=list[int2]
             button.update(disabled=False)
 
+def update_score():
+    global word
+    global score
+    for c in word:
+        to_add = scores[c]
+        score += to_add
+    score_field.update(value=score)
+
+
 def check_word():
     global word
     global word_field
@@ -117,10 +134,12 @@ def check_word():
 
     if wordnet.synsets(word):
         info_field.update(value="Nice work!")
+        update_score()
     else:
         info_field.update(value="Sorry, that's not a real word.")
     word = ""
     word_field.update(value=word)
+
 
 
 create_grid()
@@ -128,8 +147,9 @@ grid_to_buttons()
 
 word_field = sg.Text(word, size=(25, 1), background_color="#FFFFFF", text_color="#0352fc")
 info_field = sg.Text("Input a word.")
+score_field = sg.Text(str(score))
 
-layout = [[info_field], [word_field], grid, [sg.Button('Clear'), sg.Button('Submit')]]
+layout = [[score_field], [info_field], [word_field], grid, [sg.Button('Clear'), sg.Button('Submit')]]
 window = sg.Window(title="Wordify", layout=layout, margins=(100, 50), finalize=True)
 
 
